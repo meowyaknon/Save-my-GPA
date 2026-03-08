fc5ed0d6cc31a8b26b58e6d697f2adb244b56cc0
MeowYakNon <sirithep90@gmail.com>
2026-03-08 20:29:33 +0700

    Update Player.java

--- src/main/java/com/savemygpa/player/Player.java
+++ src/main/java/com/savemygpa/player/Player.java
@@ -1,4 +1,29 @@
 package com.savemygpa.player;
 
 public class Player {
+    private int energy;
+    private int intelligence;
+    private int mood;
+
+    public Player(int energy, int intelligence, int mood) {
+        this.energy = energy;
+        this.intelligence = intelligence;
+        this.mood = mood;
+    }
+
+    public int getStat(StatType type) {
+        return switch (type) {
+            case ENERGY -> energy;
+            case INTELLIGENCE -> intelligence;
+            case MOOD -> mood;
+        };
+    }
+
+    public void changeStat(StatType type, int amount) {
+        switch (type) {
+            case ENERGY -> energy += amount;
+            case INTELLIGENCE -> intelligence += amount;
+            case MOOD -> mood += amount;
+        }
+    }
 }
