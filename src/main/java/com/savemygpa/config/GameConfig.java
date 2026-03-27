07be4d938c8e84a03f07170bde27a7c480ff1c23
Emma <pearoof555@gmail.com>
2026-03-27 20:28:10 +0700

    change time config alittle

--- src/main/java/com/savemygpa/config/GameConfig.java
+++ src/main/java/com/savemygpa/config/GameConfig.java
@@ -13,8 +13,8 @@ public final class GameConfig {
     public static final int KLLC_TIME_COST = 2;
     public static final int CANTEEN_TIME_COST = 2;
     public static final int AUDITORIUM_TIME_COST = 3;
-    public static final int RELAX_TIME_COST = 1;
-    public static final int REVIEW_TIME_COST = 2;
+    public static final int RELAX_TIME_COST = 2;
+    public static final int REVIEW_TIME_COST = 1;
     public static final int EXAM_TIME_COST = 4;
 
     private GameConfig() {}
