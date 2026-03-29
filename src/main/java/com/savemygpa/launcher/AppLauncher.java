bfc39c05fd98bb7c0ea1be3dad03d0f326a5f4c0
H.M. <jame.jame9424@gmail.com>
2026-03-29 15:45:13 +0700

    update

---
+++ src/main/java/com/savemygpa/launcher/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.savemygpa.launcher;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        // เรียก Main ของ GameLauncher ที่มี JavaFX
+        GameLauncher.main(args);
+    }
+}
